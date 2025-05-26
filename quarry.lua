5ba0e0dbd2adcfd9676ce5ce212b38b4fe902a9b
WiDuMu <97642890+WiDuMu@users.noreply.github.com>
2025-05-26 01:33:51 -0500

    trying a new quarry

---
+++ quarry.lua
@@ -0,0 +1,46 @@
+local turtle = require("turtle")
+
+local x,y,z = 0,0,0 -- Coords of the turtle
+
+local boundX, boundZ = 0,0 -- This program 
+
+function refuelInPlace ()
+    local result = false
+    local slot = turtle.getSelectedSlot()
+    for i = 1, 16 do
+        turtle.select(i)
+        if turtle.refuel() then
+            result = true
+        end
+    end
+    turtle.select(slot)
+    return result
+end
+
+function refuelByReturning ()
+    while x > 0 do
+        turtle.back()
+    end
+    turtle.turnLeft()
+    while z > 0 do
+        turtle.forward()
+    end
+    while y > 0 do 
+        turtle.up()
+    end
+    turtle.turnRight()
+end
+
+function refuel ()
+    refuelInPlace()
+    if turtle.getFuelLevel < 100 then
+        refuelByReturning()
+    end
+end
+
+while true do
+   if turtle.getFuelLevel() < 100 then
+      refuel()
+   else
+
+end
